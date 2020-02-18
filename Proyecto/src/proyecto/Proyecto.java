5747dd32ce95a216f963dd06e6dee3d376fb30c1
JDGAARRAGA2000 <61156440+JDGAARRAGA2000@users.noreply.github.com>
2020-02-17 21:37:58 -0500

    Add files via upload

---
+++ Proyecto/src/proyecto/Proyecto.java
@@ -0,0 +1,15 @@
+
+package proyecto;
+
+
+public class Proyecto {
+
+   
+    public static void main(String[] args) 
+    {
+        FrmAdivina frmAdivina=new FrmAdivina();
+       //frmAdivina.show();
+       frmAdivina.setVisible(true);
+    }
+    
+}
